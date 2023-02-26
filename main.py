7a74b3fac5fa45f27a6373c717c1c8643f5c6e8c
sammiee5311 <sammiee5311@gmail.com>
2023-02-26 15:16:22 +0900

    Init fastapi for chatgpt

--- main.py
+++ main.py
@@ -1,17 +1,64 @@
+from __future__ import annotations
+
 import os
+
+import openai
+import uvicorn
 from dotenv import load_dotenv
-
-from utils.log import logger
-
+from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import status
+from pydantic import BaseModel
 
 load_dotenv(dotenv_path=".env")
 
 HOST = os.environ.get("HOST")
+PORT = int(os.environ.get("PORT"))
+OPENAPI_KEY = os.environ.get("OPENAPI_KEY")
+
+app = FastAPI()
 
 
-def main() -> None:
-    logger.info("Completed cookiecutter.")
+class Prompt(BaseModel):
+    text: str
+
+
+class Response(BaseModel):
+    text: str
+    message: str
+
+
+openai.api_key = OPENAPI_KEY
+
+
+@app.post("/ask")
+async def send_ask(prompt: Prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt.text,
+        max_tokens=4000,
+        temperature=0.5,
+    )
+
+    if not response.choices:
+        raise HTTPException(
+            dict(text="Open AI did not respond output from your prompt text. Please try again.", message="empty")
+        )
+
+    try:
+        response_text = response.choices[0].get("text").strip()
+    except AttributeError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail=dict(text="Something went wrong.", message="error")
+        )
+
+    return Response(text=response_text, message="success")
+
+
+@app.get("/")
+async def main():
+    return Response(text="This is backend for chatGPT.", message="success")
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host=HOST, port=PORT)
